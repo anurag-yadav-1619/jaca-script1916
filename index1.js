6a4d54be572a7c9ee32a6da330f8cad13ad62435
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-03-10 01:23:09 +0530

    js

---
+++ index1.js
@@ -0,0 +1,16 @@
+// let  neam="anurag";
+// let age=20;
+// age =70;
+// console.log(neam,age);
+// const account=5424;
+// account
+// console.log(account);
+// var a=10;
+// var a=30;
+// if(true){
+//     var a=60;
+// }
+// console.log(a);
+ 
+
+
---
+++ index2.js
@@ -0,0 +1,11 @@
+// data types 
+// primitive data types: there are seven data types:-
+// number ,string,booline,undefine,null,bigint,symbol
+//number 
+ let a=10;
+ let b=98.8;
+ console.log(a,b);
+
+
+// non primitive data types are:-
+//array,object,function
