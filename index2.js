7d8fdc734a4bf54cb7faab90aca959a69fe9da25
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-03-31 14:28:22 +0530

    js

--- index2.js
+++ index2.js
@@ -2,10 +2,59 @@
 // primitive data types: there are seven data types:-
 // number ,string,booline,undefine,null,bigint,symbol
 //number 
- let a=10;
- let b=98.8;
- console.log(a,b);
+//  let a=10;
+//  let b=98.8;
+//  console.log(typeof b);
+//  console.log(a,b);
+// // string
+// let c="anurag";
+// console.log(c);
+// //boolian
+// let login=true;
+// let f=false;
+// console.log(login,f)
+// //undefine
+// let user;
+
+// console.log(user); 
+// //bigint
+// let num=2345324533243252423432532n;
+// console.log(num);
+// //numll
+// let weather=null;
+// console.log(weather); 
+// //symbol
+// const id1=Symbol("id");
+// console.log(id1);
+// const id2=Symbol("id");
+// console.log(id2);
+ 
 
 
 // non primitive data types are:-
-//array,object,function
+//array,object,function:-
+//array
+ //let arr =[10,19,13,"anurag",false,true];
+ //console.log(typeof arr);
+// console.log(arr);
+// //object
+// let obj={
+//     name:"anurag yadav",
+//     account:1619,
+//     age:20,
+//     category:"gen",
+//     area:"mau"
+// }
+// //function
+// let s=function add(){
+//     console.log("anurag");
+// } 
+// s();
+// let arr=[10,19,345,54,3,4,324,18,"anurag",false,true,"you"];
+// console.log(arr);
+let str="Rohit";
+console.log(str[0]);
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+// data types 
+// primitive data types: there are seven data types:-
+// number ,string,booline,undefine,null,bigint,symbol
+//number 
+ let a=10;
+ let b=98.8;
+ console.log(a,b);
+// string
+let c="anurag";
+console.log(c);
+//boolian
+let login=true;
+let f=false;
+console.log(login,f)
+
+// non primitive data types are:-
+//array,object,function
