3a15a67fa90033735625b609ac1055f5f5437f2a
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-04-02 01:26:30 +0530

    js

---
+++ indes4.js
@@ -0,0 +1,31 @@
+console.log("this is anurag")
+
+// let a=5;
+// let b=9;
+// console.log(a+b)
+
+// if((a+b)>18){
+//     console.log("yes");
+    
+// }
+// else{
+//     console.log("no");
+// }
+
+       
+
+// let x=3;
+// let y=5;
+// x+=y
+// console.log(x)
+
+a=12
+if(a=18){
+    console.log("yes")
+} 
+else if(a!=18) {
+    console.log("are you ch**")
+}
+else{
+    console.log("no")
+}
---
+++ loops.js
@@ -0,0 +1,36 @@
+console.log("i am anurag ")
+let a=1   
+for(let i=0;i<10;i++){
+    console.log(i)
+}
+
+let  obj={
+    name: "anurag  yadav",
+    role: "student",
+    company:"anurag AI"
+}
+for (const key in obj) {
+    const element=obj[key];
+    console.log(key,element)
+
+}
+for (const c of "anurag") {
+    console.log(c)
+    
+}
+while loop
+
+
+
+let i=0;
+while (i<=6) {
+    console.log(i)
+    i++;
+}
+
+do while loop
+let i=6
+do {
+    console.log(i)
+    
+} while ();
