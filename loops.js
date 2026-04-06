75d2846707cf52daa4bf517e50a6bfa329358832
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-04-06 13:18:57 +0530

    ecommars

--- loops.js
+++ loops.js
@@ -1,36 +1,36 @@
-console.log("i am anurag ")
-let a=1   
-for(let i=0;i<10;i++){
-    console.log(i)
-}
+// console.log("i am anurag ")
+// let a=1   
+// for(let i=0;i<10;i++){
+//     console.log(i)
+// }
 
-let  obj={
-    name: "anurag  yadav",
-    role: "student",
-    company:"anurag AI"
-}
-for (const key in obj) {
-    const element=obj[key];
-    console.log(key,element)
+// let  obj={
+//     name: "anurag  yadav",
+//     role: "student",
+//     company:"anurag AI"
+// }
+// for (const key in obj) {
+//     const element=obj[key];
+//     console.log(key,element)
 
-}
-for (const c of "anurag") {
-    console.log(c)
+// }
+// for (const c of "anurag") {
+//     console.log(c)
     
-}
-while loop
+// }
+// while loop
 
 
 
-let i=0;
-while (i<=6) {
-    console.log(i)
-    i++;
-}
+// let i=0;
+// while (i<=6) {
+//     console.log(i)
+//     i++;
+// }
 
-do while loop
-let i=6
-do {
-    console.log(i)
+// do while loop
+// let i=6
+// do {
+//     console.log(i)
     
-} while ();
+// } while ();
---
+++ nmuber.js
@@ -0,0 +1,10 @@
+// let a=10
+// console.log(a)
+// let b=3453.543543;
+// console.log(b.toFixed(4))
+// console.log(b.toPrecision(5))
+// console.log(b.toString())
+
+
+let a=new Number(20)
+console.log(a)
