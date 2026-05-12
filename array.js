6839a9a11725392b053bd57941a734b016b47ad8
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-05-12 22:34:40 +0530

    js

---
+++ array.js
@@ -0,0 +1,44 @@
+let arr=[1,2,3,4,5,6]
+console.log(arr)
+console.log(arr.length)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr.toString())
+console.log()
+arr.push("anurag")
+//  let a1=[1,2,3,4]
+//  let a2=[5,6,7]
+//  let a3=[8,9,10]
+//  a4.concat(a1,a2)
+// console.log(a4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ loop.js
@@ -0,0 +1,7 @@
+let a=[1,23,4,5,]
+for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    console.log(element)
+
+    
+}
