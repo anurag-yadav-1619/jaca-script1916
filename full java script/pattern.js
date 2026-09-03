14331c2cba7592eceb0923182d51cee7d3b4e521
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-09-03 11:42:09 +0530

    js

---
+++ full java script/pattern.js
@@ -0,0 +1,49 @@
+// let n = 4;
+
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+
+//     for (let j = 1; j <= i; j++) {
+//         row += "* ";
+//     }
+
+//     console.log(row);
+// }
+
+
+let n = 4;
+
+for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    for (let j = 1; j <= i; j++) {
+        row += j+ " ";
+    }
+
+    console.log(row);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
