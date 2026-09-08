eedd1c3cd6122761a3e451f68a80ead07d0ec23d
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-09-08 11:35:27 +0530

    java

--- full java script/restoperaterin.js
+++ full java script/restoperaterin.js
@@ -4,13 +4,80 @@
 // sum(9,12)
 // sum(100,199,78,987,4,46)
 
-function sum(...value){
-    let total=0;
-    for(let n of value){
-        total=total+n;
-    }
-    return total;
+// function sum(...value){
+//     let total=0;
+//     for(let n of value){
+//         total=total+n;
+//     }
+//     return total; 
 
-}
+// }
 
-console.log(sum(34,54,6,9,34,56,78,45,76))
+// console.log(sum(34,54,6,9,34,56,78,45,76))
+
+
+// function sum(... values){
+//     let sum=0;
+    
+//     for( let i in values)
+//     {
+//         sum=sum+values[i];
+//     }
+//     console.log(sum)
+// };
+// let as =[10,20,30,40]
+// sum(...as)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
