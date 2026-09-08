c633c7d56b6647553e2910f6620d71e2fd60aee2
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-09-08 12:16:11 +0530

    java

--- full java script/restoperaterin.js
+++ full java script/restoperaterin.js
@@ -29,26 +29,45 @@
 // sum(...as)
 
 
-//             object litrals
-let name='anurag'
-let rollnumber=101
-let data={
-    name,
-    rollnumber
-}
-console.log(data.name)
-console.log(data.rollnumber)
+// //                              object litrals
+// let name='anurag'
+// let rollnumber=101
+// let data={
+//     name,
+//     rollnumber
+// }
+// console.log(data.name)
+// console.log(data.rollnumber)
+
+// let F="Firstname"
+// let L="Lastname"
+// let data={
+//     [F]:"anurag",
+//     [L]:'yadav'
+// }
+// console.log(data.Firstname)
+// console.log(data.Lastname)
 
 
 
+// let F = "Firstname";
+// let L = "Lastname";
+
+// let data = {
+//     [F]: "anurag",
+//     [L]: "yadav",
+
+//     Show() {
+//         console.log(this.Firstname);
+//         console.log(this.Lastname);
+//     }
+// };
+
+// data.Show();
 
 
 
-
-
-
-
-
+//                                 
 
 
 
